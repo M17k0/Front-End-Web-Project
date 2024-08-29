0c904e13c7518226004992b19c83acc94c567275
Dimitar Donkov <m17k018@gmail.com>
2024-08-29 20:39:51 +0300

    Fix auth title

--- scripts/toggle-forms.js
+++ scripts/toggle-forms.js
@@ -2,9 +2,11 @@ function toggleAuthForms(formType) {
   if (formType === 'login') {
     document.getElementById('login-container').style.display = 'block';
     document.getElementById('register-container').style.display = 'none';
+    document.title = 'Login';
   } else {
     document.getElementById('login-container').style.display = 'none';
     document.getElementById('register-container').style.display = 'block';
+    document.title = 'Register';
   }
 }
 
